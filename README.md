c9c40dc6e470a97b427514a7f838df3769b3acb0
Kushagra Gupta <kushagra.kingg@live.com>
2023-05-25 15:07:04 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# lazyVim
+ LazyVim Config
---
+++ lua/plugins/lspcustom.lua
@@ -0,0 +1,58 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "clangd",
+        "rust-analyzer",
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      -- add tsx and treesitter
+      vim.list_extend(opts.ensure_installed, {
+        "tsx",
+        "typescript",
+        "c",
+        "rust",
+        "javascript",
+      })
+    end,
+  },
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function()
+      vim.g.rustfmt_autosave = 1
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "simrat39/rust-tools.nvim",
+    },
+    opts = {
+      servers = {
+        rust_analyzer = {},
+      },
+      setup = {
+        rust_analyzer = function(_, opts)
+          require("rust-tools").setup({ server = opts })
+          return true
+        end,
+      },
+    },
+  },
+  {
+    "saecki/crates.nvim",
+    ft = { "rust", "toml" },
+    config = function(_, opts)
+      local crates = require("crates")
+      crates.setup(opts)
+      crates.show()
+    end,
+  },
+}
