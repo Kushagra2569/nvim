d10ac67219faf80f0da4af203cefe25d9618c5b9
Kushagra Gupta <kushagra.kingg@live.com>
2024-04-29 03:57:59 +0530

    added transparency effect to float and nvim tree windows

--- lua/plugins/customMisc.lua
+++ lua/plugins/customMisc.lua
@@ -32,7 +32,10 @@ return {
           "CursorLineNr",
           "EndOfBuffer",
         },
-        extra_groups = {}, -- table: additional groups that should be cleared
+        extra_groups = {
+          "NormalFloat", -- plugins which have float panel such as Lazy, Mason, LspInfo
+          "NvimTreeNormal",
+        }, -- table: additional groups that should be cleared
         exclude_groups = {}, -- table: groups you don't want to clear
       })
     end,
